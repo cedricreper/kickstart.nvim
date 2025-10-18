801f3a41b08a221949d31ed3e2217aa10f6cc04c
cedricreper <cedric@makeleaps.com>
2025-10-18 17:11:16 +0900

    fix colorscheme config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,24 @@
 return {
   {
-    --    'sainnhe/gruvbox-material',
-    --    lazy = false,
-    --    priority = 1000,
-    --    config = function()
-    --      vim.o.background = 'dark'
-    --      vim.g.gruvbox_material_enable_italic = true
-    --      vim.g.gruvbox_material_background = 'hard'
-    --      vim.cmd.colorscheme 'gruvbox-material'
-    --    end,
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = 'dark'
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_background = 'hard'
+      -- vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+  {
+    'oahlen/iceberg.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd 'colorscheme iceberg'
+    end,
+  },
+  {
     'webhooked/kanso.nvim',
     lazy = false,
     priority = 1000,
@@ -16,46 +26,51 @@ return {
       require('kanso').setup {
         transparent = false,
       }
-      vim.cmd 'colorscheme kanso'
+      -- vim.cmd 'colorscheme kanso'
     end,
-    -- 'catppuccin/nvim',
-    --  name = 'catppuccin',
-    -- priority = 1000,
-    --  config = function()
-    --    require('catppuccin').setup {
-    --      flavour = 'mocha',
-    --      transparent_background = true,
-    --   }
-    --   vim.cmd 'colorscheme catppuccin'
-    -- end,
-    -- 'rebelot/kanagawa.nvim',
-    -- priority = 1000, -- Make sure to load this before all the other start plugins.
-    -- name = 'kanagawa-dragon',
-    -- config = function()
-    --   ---@diagnostic disable-next-line: missing-fields
-    --   require('kanagawa').setup {
-    --     commentStyle = { italic = true },
-    --     -- statementStyle = { bold = false },
-    --     theme = 'dragon',
-    --   }
-    --   vim.cmd 'colorscheme kanagawa-dragon'
-    -- end,
-    --'zenbones-theme/zenbones.nvim',
-    --  dependencies = 'rktjmp/lush.nvim',
-    -- lazy = false,
-    -- priority = 1000,
-    -- config = function()
-    --   vim.cmd 'colorscheme zenbones'
-    -- end,
-    -- 'vague2k/vague.nvim',
-    -- priority = 1000,
-    -- name = 'vague',
-    -- config = function()
-    --   -- NOTE: you do not need to call setup if you don't want to.
-    --   require('vague').setup {
-    --     -- optional configuration here
-    --   }
-    --   vim.cmd 'colorscheme vague'
-    -- end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'mocha',
+        transparent_background = false,
+      }
+      -- vim.cmd 'colorscheme catppuccin'
+    end,
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('kanagawa').setup {
+        commentStyle = { italic = true },
+        theme = 'dragon',
+      }
+      -- vim.cmd 'colorscheme kanagawa-dragon'
+    end,
+  },
+  {
+    'zenbones-theme/zenbones.nvim',
+    dependencies = 'rktjmp/lush.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.cmd 'colorscheme zenbones'
+    end,
+  },
+  {
+    'vague2k/vague.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('vague').setup {}
+      -- vim.cmd 'colorscheme vague'
+    end,
   },
 }
