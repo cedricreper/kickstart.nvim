87bfb0569b3a88a47b0389e54141b7eb2245d882
cedricreper <cedric@makeleaps.com>
2026-03-31 10:02:14 +0900

    save config before 0.12 upgrade

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -9,8 +9,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.api.nvim_create_user_command('Cur', function()
-  local path = vim.fn.fnamemodify(vim.fn.expand '%:p:h', ':t') .. '/' .. vim.fn.expand '%:t'
+vim.api.nvim_create_user_command('Path', function()
+  local filepath = vim.fn.expand '%:p'
+  local filedir = vim.fn.expand '%:p:h'
+  local git_dir = vim.fn.finddir('.git', filedir .. ';')
+
+  local path
+  if git_dir ~= '' then
+    -- Found .git directory, get path relative to repo root
+    local root = vim.fn.fnamemodify(git_dir, ':p:h:h')
+    path = filepath:gsub('^' .. vim.pesc(root) .. '/', '')
+  else
+    -- Fallback to just filename if not in git repo
+    path = vim.fn.expand '%:t'
+  end
+
   vim.fn.setreg('+', path) -- copy to system clipboard
   print('Copied: ' .. path)
 end, {})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,53 @@
 return {
   {
+    'ydkulks/cursor-dark.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'cursor-dark'
+    end,
+  },
+  {
+    'vague-theme/vague.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    config = function()
+      -- NOTE: you do not need to call setup if you don't want to.
+      require('vague').setup {
+        -- optional configuration here
+        bold = false,
+        italic = false,
+      }
+      --      vim.cmd 'colorscheme vague'
+    end,
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('gruvbox').setup {
+        bold = false,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = true,
+          operators = false,
+          folds = false,
+        },
+        contrast = 'soft', -- can be "hard", "soft" or empty string
+      }
+      --vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+  {
     'sainnhe/gruvbox-material',
     lazy = false,
     priority = 1000,
     config = function()
       vim.o.background = 'dark'
-      vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_enable_italic = false
+      -- vim.g.gruvbox_material_background = 'hard'
       -- vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
@@ -15,7 +56,16 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd 'colorscheme iceberg'
+      --vim.cmd 'colorscheme iceberg'
+    end,
+  },
+  {
+    'shaunsingh/nord.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.g.nord_italic = false
+      -- vim.cmd 'colorscheme nord'
     end,
   },
   {
@@ -39,7 +89,7 @@ return {
         flavour = 'mocha',
         transparent_background = false,
       }
-      -- vim.cmd 'colorscheme catppuccin'
+      vim.cmd 'colorscheme catppuccin'
     end,
   },
   {
@@ -65,12 +115,11 @@ return {
     end,
   },
   {
-    'vague2k/vague.nvim',
-    lazy = false,
-    priority = 1000,
+    'anAcc22/sakura.nvim',
+    dependencies = 'rktjmp/lush.nvim',
     config = function()
-      require('vague').setup {}
-      -- vim.cmd 'colorscheme vague'
+      vim.opt.background = 'dark' -- or "light"
+      -- vim.cmd 'colorscheme sakura' -- sets the colorscheme
     end,
   },
 }
